b8556ca2bdec659dfc8add4f0fe4b26f3c849b5c
lydieuwayezu <uwayezulydie1w@gmail.com>
2026-02-18 15:22:36 +0200

    pop ,push ,shift,unshift and splice

--- script.js
+++ script.js
@@ -7,3 +7,33 @@ console.log("Total:", productCategories.length);
 productCategories[1] = "Stationery";
 console.log("Modified array:", productCategories);
 
+
+
+
+
+
+
+let inventory = [10, 20, 30];
+console.log(inventory);
+
+inventory.push(40);
+console.log("After push:", inventory);
+
+inventory.pop();
+console.log("After pop:", inventory);
+
+inventory.unshift(0);
+console.log("After unshift:", inventory);
+
+inventory.shift();
+console.log("After shift:", inventory);
+
+inventory.splice(1, 1, 25, 35);
+console.log("After splice:", inventory);
+
+
+
+
+
+
+
