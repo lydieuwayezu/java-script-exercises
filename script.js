17326586157b21acfeb27b41ae7138f6e70d4ba0
lydieuwayezu <uwayezulydie1w@gmail.com>
2026-02-18 15:06:38 +0200

    change

---
+++ script.js
@@ -0,0 +1,9 @@
+let productCategories = ["Electronics", "Books", "Clothes", "Shoes", "Food"];
+
+console.log("First:", productCategories[0]);
+console.log("Last:", productCategories[productCategories.length - 1]);
+console.log("Total:", productCategories.length);
+
+productCategories[1] = "Stationery";
+console.log("Modified array:", productCategories);
+
